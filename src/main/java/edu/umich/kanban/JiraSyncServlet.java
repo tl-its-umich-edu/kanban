90f9d73f3c7d0858c73e7460380f71f9ff93f71b
bkirschn <bkirschn@5469654b-834f-431e-a963-8df4f965c24f>
2014-01-28 23:27:25 +0000

    formatting changes for consistency
    git-svn-id: https://wush.net/svn/ctools/kanban/trunk@2061 5469654b-834f-431e-a963-8df4f965c24f

--- src/main/java/edu/umich/kanban/JiraSyncServlet.java
+++ src/main/java/edu/umich/kanban/JiraSyncServlet.java
@@ -18,62 +18,62 @@ import java.util.logging.Logger;
  * Servlet implementation class JiraSyncServlet
  */
 public class JiraSyncServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static Log M_log = LogFactory.getLog(JiraSyncServlet.class);
-
+   private static final long serialVersionUID = 1L;
+   private static Log M_log = LogFactory.getLog(JiraSyncServlet.class);
    
-    public JiraSyncServlet() {
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter out =response.getWriter();
-		String queryString = request.getQueryString();
-                ResourceBundle props = ResourceBundle.getBundle("jira");
-		
-		XmlMerge xmlMerge = new XmlMerge();
-		
-		if (queryString.equals("OK")) {
-			String mergeFiles="";
-			try {
-	                    String xmlPath = props.getString("xml.path");
-			    String xsltPath = props.getString("xslt.path");
-			    mergeFiles=xmlMerge.mergeFiles(queryString, 
-							   getServletContext().getRealPath(xsltPath),
-							   getServletContext().getRealPath(xmlPath));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			dataOutput(out, mergeFiles);
-			}
-			if(queryString.equals("wip")) {
-				StringBuilder jsonWip=new StringBuilder("[ {");
-				jsonWip.append("\"todo\" : \"")
-				       .append(props.getString("wip.todo"))
-				       .append("\"");
-				jsonWip.append(", \"inprogress\" : \"");
-				jsonWip.append(props.getString("wip.inprogress")).append("\"");
-				jsonWip.append(", \"review\" : \"");
-				jsonWip.append(props.getString("wip.review")).append("\"");
-				jsonWip.append("} ]");
-				dataOutput(out, jsonWip.toString());
-			}
-		}
-		
-		private void dataOutput(PrintWriter out, String mergeFiles) {
-			out.println(mergeFiles);
-			M_log.debug("merged json: "+mergeFiles);
-			out.close();
-		}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
+   
+   public JiraSyncServlet() {
+   }
+   
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException {
+      response.setContentType("text/html");
+      PrintWriter out =response.getWriter();
+      String queryString = request.getQueryString();
+      ResourceBundle props = ResourceBundle.getBundle("jira");
+      
+      XmlMerge xmlMerge = new XmlMerge();
+      
+      if (queryString.equals("OK")) {
+         String mergeFiles="";
+         try {
+            String xmlPath = props.getString("xml.path");
+            String xsltPath = props.getString("xslt.path");
+            mergeFiles=xmlMerge.mergeFiles(queryString, 
+                                           getServletContext().getRealPath(xsltPath),
+                                           getServletContext().getRealPath(xmlPath));
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+         dataOutput(out, mergeFiles);
+      }
+      if(queryString.equals("wip")) {
+         StringBuilder jsonWip=new StringBuilder("[ {");
+         jsonWip.append("\"todo\" : \"")
+            .append(props.getString("wip.todo"))
+            .append("\"");
+         jsonWip.append(", \"inprogress\" : \"");
+         jsonWip.append(props.getString("wip.inprogress")).append("\"");
+         jsonWip.append(", \"review\" : \"");
+         jsonWip.append(props.getString("wip.review")).append("\"");
+         jsonWip.append("} ]");
+         dataOutput(out, jsonWip.toString());
+      }
+   }
+   
+   private void dataOutput(PrintWriter out, String mergeFiles) {
+      out.println(mergeFiles);
+      M_log.debug("merged json: "+mergeFiles);
+      out.close();
+   }
+   
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+   }
+   
 }
