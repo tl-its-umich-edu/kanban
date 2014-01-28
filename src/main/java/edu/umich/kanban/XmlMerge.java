50b96f24b7f5bd03a34b90c6a6d3c1bb5f67c186
bkirschn <bkirschn@5469654b-834f-431e-a963-8df4f965c24f>
2014-01-28 23:30:34 +0000

    formatting changes for consistency
    git-svn-id: https://wush.net/svn/ctools/kanban/trunk@2062 5469654b-834f-431e-a963-8df4f965c24f

--- src/main/java/edu/umich/kanban/XmlMerge.java
+++ src/main/java/edu/umich/kanban/XmlMerge.java
@@ -29,99 +29,98 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 public class XmlMerge {
-	
 
-    /**
-     ** Merge and transform the two JIRA XML file streams defined by the its.url and sakai.url properties
-     ** @param queryString Servlet query string (currently not used)
-     ** @param xsltPath Input filepath of XSLT to transform xml into JSON
-     ** @param xmlPath Output filepath of merged XML
-     ** @returns String XML file transformed into JSON
-     **/
-    public String mergeFiles(String queryString, String xsltPath, String xmlPath) throws Exception {
-		
-		ResourceBundle labels = ResourceBundle.getBundle("jira");
-		String its=basicAuthITS(labels);
-		String sakai = basicAuthSakai(labels);
-		
-		//merging 2 Xml file from both ITS and Sakai Xml to single file using DOm parsing
-		DocumentBuilderFactory domFactory = DocumentBuilderFactory
-				.newInstance();
-		domFactory.setIgnoringComments(true);
-		DocumentBuilder builder = domFactory.newDocumentBuilder();
-		Document doc = builder.parse(new InputSource(new StringReader(sakai)));
-		Document docIts = builder.parse(new InputSource(new StringReader(its)));
+   /**
+    ** Merge and transform the two JIRA XML file streams defined by the its.url and sakai.url properties
+    ** @param queryString Servlet query string (currently not used)
+    ** @param xsltPath Input filepath of XSLT to transform xml into JSON
+    ** @param xmlPath Output filepath of merged XML
+    ** @returns String XML file transformed into JSON
+    **/
+   public String mergeFiles(String queryString, String xsltPath, String xmlPath) throws Exception {
       
-		NodeList nodesSakai = doc.getElementsByTagName("item");
-		NodeList nodesIts = docIts.getElementsByTagName("item");
-		for (int i = 0; i < nodesIts.getLength(); i++) {
-			 Node n = (Node) doc.importNode(nodesIts.item(i), true);
-			 nodesSakai.item(0).getParentNode().appendChild(n);
-		}
-		Transformer transformer = TransformerFactory.newInstance()
-				.newTransformer();
-		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-		StreamResult result = new StreamResult(new StringWriter());
-		DOMSource xmlSource = new DOMSource(doc);
-		transformer.transform(xmlSource, result);
-		
-		Writer output = output = new BufferedWriter(new FileWriter(xmlPath));
-		String xmlOutput = result.getWriter().toString();
-		output.write(xmlOutput);
-		output.close();
-		
-		// XSLT code - transform merged XMLfile into JSON
-		TransformerFactory factory = TransformerFactory.newInstance();
-		Source xsltSource = new StreamSource(new File(xsltPath));
-		Transformer newTransformer = factory.newTransformer(xsltSource);
-		StreamResult result1 = new StreamResult(new StringWriter());
-		
-		newTransformer.transform(xmlSource, result1);
-		String jsonOutput = result1.getWriter().toString();
-		return jsonOutput;
-	}
-
-		//getting ITS data using basic Auth
-private String basicAuthITS(ResourceBundle its) throws Exception{
-		
-		String webPage =its.getString("its.url");
-		String name =its.getString("its.username");
-		String password = its.getString("its.password");
-		String authString=name+":"+password;
-		byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
-		String authStringEnc = new String(authEncBytes);
-		URL url = new URL(webPage);
-		URLConnection urlConnection = url.openConnection();
-		urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
-		InputStream is = urlConnection.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is);
-		int numCharsRead;
-		char[] charArray = new char[1024];
-		StringBuffer sb = new StringBuffer();
-		while ((numCharsRead = isr.read(charArray)) > 0) {
-			sb.append(charArray, 0, numCharsRead);
-		}
-		String result = sb.toString();
-		return result;
-	}
-    //getting the Sakai data 
-private String basicAuthSakai(ResourceBundle sakai) throws Exception{
-	
-	String webPage = sakai.getString("sakai.url");
-	URL url = new URL(webPage);
-	URLConnection urlConnection = url.openConnection();
-	InputStream is = urlConnection.getInputStream();
-	InputStreamReader isr = new InputStreamReader(is);
-	int numCharsRead;
-	char[] charArray = new char[1024];
-	StringBuffer sb = new StringBuffer();
-	while ((numCharsRead = isr.read(charArray)) > 0) {
-		sb.append(charArray, 0, numCharsRead);
-	}
-	String result = sb.toString();
-	return result;
-	
-	
-}
-
+      ResourceBundle labels = ResourceBundle.getBundle("jira");
+      String its=basicAuthITS(labels);
+      String sakai = basicAuthSakai(labels);
+      
+      //merging 2 Xml file from both ITS and Sakai Xml to single file using DOm parsing
+      DocumentBuilderFactory domFactory = DocumentBuilderFactory
+         .newInstance();
+      domFactory.setIgnoringComments(true);
+      DocumentBuilder builder = domFactory.newDocumentBuilder();
+      Document doc = builder.parse(new InputSource(new StringReader(sakai)));
+      Document docIts = builder.parse(new InputSource(new StringReader(its)));
+      
+      NodeList nodesSakai = doc.getElementsByTagName("item");
+      NodeList nodesIts = docIts.getElementsByTagName("item");
+      for (int i = 0; i < nodesIts.getLength(); i++) {
+         Node n = (Node) doc.importNode(nodesIts.item(i), true);
+         nodesSakai.item(0).getParentNode().appendChild(n);
+      }
+      Transformer transformer = TransformerFactory.newInstance()
+         .newTransformer();
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      StreamResult result = new StreamResult(new StringWriter());
+      DOMSource xmlSource = new DOMSource(doc);
+      transformer.transform(xmlSource, result);
+      
+      Writer output = output = new BufferedWriter(new FileWriter(xmlPath));
+      String xmlOutput = result.getWriter().toString();
+      output.write(xmlOutput);
+      output.close();
+      
+      // XSLT code - transform merged XMLfile into JSON
+      TransformerFactory factory = TransformerFactory.newInstance();
+      Source xsltSource = new StreamSource(new File(xsltPath));
+      Transformer newTransformer = factory.newTransformer(xsltSource);
+      StreamResult result1 = new StreamResult(new StringWriter());
+      
+      newTransformer.transform(xmlSource, result1);
+      String jsonOutput = result1.getWriter().toString();
+      return jsonOutput;
+   }
+   
+   //getting ITS data using basic Auth
+   private String basicAuthITS(ResourceBundle its) throws Exception {
+      
+      String webPage =its.getString("its.url");
+      String name =its.getString("its.username");
+      String password = its.getString("its.password");
+      String authString=name+":"+password;
+      byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+      String authStringEnc = new String(authEncBytes);
+      URL url = new URL(webPage);
+      URLConnection urlConnection = url.openConnection();
+      urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+      InputStream is = urlConnection.getInputStream();
+      InputStreamReader isr = new InputStreamReader(is);
+      int numCharsRead;
+      char[] charArray = new char[1024];
+      StringBuffer sb = new StringBuffer();
+      while ((numCharsRead = isr.read(charArray)) > 0) {
+         sb.append(charArray, 0, numCharsRead);
+      }
+      String result = sb.toString();
+      return result;
+   }
+   
+   //getting the Sakai data 
+   private String basicAuthSakai(ResourceBundle sakai) throws Exception {
+      
+      String webPage = sakai.getString("sakai.url");
+      URL url = new URL(webPage);
+      URLConnection urlConnection = url.openConnection();
+      InputStream is = urlConnection.getInputStream();
+      InputStreamReader isr = new InputStreamReader(is);
+      int numCharsRead;
+      char[] charArray = new char[1024];
+      StringBuffer sb = new StringBuffer();
+      while ((numCharsRead = isr.read(charArray)) > 0) {
+         sb.append(charArray, 0, numCharsRead);
+      }
+      String result = sb.toString();
+      return result;
+      
+   }
+   
 }
